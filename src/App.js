eb311894c9663dbe6a87c1539db1a129662331d9
tugcekonuklar <tkonuklar@gmail.com>
2020-11-29 23:40:42 +0100

    feat: add notification

--- src/App.js
+++ src/App.js
@@ -1,48 +1,36 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
+import { ToastProvider } from 'react-toast-notifications'
 import './App.css';
 import { AppWrapper } from './components/app-wrapper/app-warpper.component';
 import { Header } from './components/header/header.component';
 import { TodoList } from './components/todo-list/todo-list.component'
-import {SubmitForm} from './components/submit-form/submit-form.component'
+import { SubmitForm } from './components/submit-form/submit-form.component'
 
 function App() {
   const [tasks, setTasks] = useState(['task1', 'task2', 'task3', 'task4']);
 
-  function handleDelete (index){
+  function handleDelete(index) {
     const newArr = [...tasks];
-    newArr.splice(index,1);
+    newArr.splice(index, 1);
     setTasks(newArr);
   }
 
-  function handleSubmit (content) {
-    setTasks([...tasks,content]);
+  function handleSubmit(content) {
+    setTasks([...tasks, content]);
   }
 
-    return (
-      <div className="App">
+  return (
+    <div className="App">
+      <ToastProvider>
         <AppWrapper>
           <Header numberTodos={tasks.length}></Header>
           <SubmitForm onCreate={handleSubmit}></SubmitForm>
           <TodoList tasks={tasks} onDeleteItem={handleDelete}></TodoList>
         </AppWrapper>
-      </div>
-    );
-  }
+      </ToastProvider>
+    </div>
+  );
+}
 
 export default App;
 
-
-
-
-{/* 
-      <AppWrapper>
-        <Header></Header>
-        <SubmitForm></SubmitForm>
-        <TodoList>
-          <Todo></Todo>
-          <Todo></Todo>
-          <Todo></Todo>
-          <Todo></Todo>
-          <Todo></Todo>
-        </TodoList>
-      </AppWrapper> */}
--- src/components/submit-form/submit-form.component.jsx
+++ src/components/submit-form/submit-form.component.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
-
+import { useToasts } from 'react-toast-notifications'
 export function SubmitForm(props) {
     const [content, setContent] = useState('');
-
+    const { addToast } = useToasts()
     function handleSubmit(e) {
         e.preventDefault();
+        addToast('Saved have a new tasks to do ' + content, { appearance: 'success' })
         props.onCreate(content);
     }
-    
+
     return (
         <div>
             <form onSubmit={handleSubmit}>
